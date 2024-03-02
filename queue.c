7957aa5f8451637e1406c535f168770643b0b020
backink <r09921093@g.ntu.edu.tw>
2024-03-03 00:37:07 +0800

    Implement reverse and delete duplicate

--- queue.c
+++ queue.c
@@ -151,6 +151,24 @@ bool q_delete_mid(struct list_head *head)
 bool q_delete_dup(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+    if (!head)
+        return false;
+    if (list_empty(head))
+        return true;
+
+    element_t *entry, *safe;
+    bool dup = false;
+    list_for_each_entry_safe (entry, safe, head, list) {
+        if (&safe->list != head && !strcmp(entry->value, safe->value)) {
+            dup = true;
+            list_del(&entry->list);
+            q_release_element(entry);
+        } else if (dup) {
+            dup = false;
+            list_del(&entry->list);
+            q_release_element(entry);
+        }
+    }
     return true;
 }
 
@@ -168,7 +186,17 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head))
+        return;
+
+    struct list_head *last = head->next;
+    struct list_head *node = head;
+    for (; head->prev != last; node = node->next) {
+        list_move(head->prev, node);
+    }
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
