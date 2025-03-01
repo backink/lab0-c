036bf924199bd948a3527a3d4d11f41a198f80ee
backink <backink1994@gmail.com>
2025-03-01 19:24:05 +0800

    Implement q_insert_head and q_remove_head
    Implemented q_insert_head and q_remove_head
    q_size is also completed
    
    Change-Id: I556b5e95d35b857820216d33e9d88773cfa03f97

--- queue.c
+++ queue.c
@@ -85,19 +85,48 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+
+    element_t *ele = list_first_entry(head, element_t, list);
+    if (ele->value && sp) {
+        strncpy(sp, ele->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    list_del(head->next);
+    return ele;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+
+    element_t *ele = list_last_entry(head, element_t, list);
+    if (ele->value && sp) {
+        strncpy(sp, ele->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    return ele;
 }
 
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head || list_empty(head)) {
+        return 0;
+    }
+
+    int ret = 0;
+    struct list_head *tmp = NULL;
+    list_for_each (tmp, head) {
+        ret++;
+    }
+    return ret;
 }
 
 /* Delete the middle node in queue */
